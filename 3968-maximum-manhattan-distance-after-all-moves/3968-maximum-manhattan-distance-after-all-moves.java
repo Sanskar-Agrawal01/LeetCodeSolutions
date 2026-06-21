085b0afb4bfb5c7655c69e1eba3ed9f6d61ecb83
Sanskar Agrawal <agrawalss_15@rknec.edu>
2026-06-21 13:27:18 +0530

    Time: 15 ms (99.87%), Space: 48 MB (26.06%) - LeetHub

---
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxDistance(String moves) {
+        int dist = 0;
+        int l = 0;
+        int r = 0;
+        int u = 0;
+        int d = 0;
+        int b = 0;
+        for(char ch : moves.toCharArray()){
+            if(ch == 'L' ) l++;
+            else if(ch == 'R') r++;
+            else if(ch == 'U' ) u++;
+            else if(ch == 'D')d++;
+            else if (ch == '_') b++;
+        }
+        dist = Math.abs(l -r) + Math.abs(u - d);
+        if(b == 0) return dist;
+        return dist + b;
+    }
+}
