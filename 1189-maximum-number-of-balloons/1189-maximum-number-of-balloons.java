39fdff2bd6df2cbf43ae8be2334ee3b2911d05c8
Sanskar Agrawal <agrawalss_15@rknec.edu>
2026-06-22 13:19:29 +0530

    Time: 4 ms (63.02%), Space: 43.2 MB (87.41%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -13,11 +13,6 @@ class Solution {
             if(ch == 'o')o++;
             if(ch == 'n')n++;
         }
-        int min = Integer.MAX_VALUE;
-        if(b > 0 && a > 0 && n > 0 && l/2 >0 && o/2 > 0){
-            min = Math.min(b, Math.min(a, Math.min(n, Math.min(l/2, o/2))));
-        }
-        if(min != Integer.MAX_VALUE) return min;
-        return 0;
+        return  Math.min(b, Math.min(a, Math.min(n, Math.min(l/2, o/2))));
     }
 }
